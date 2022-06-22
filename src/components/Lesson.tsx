09219f3549335aa0755cc1768a407782f8ce52ad
josesouzaa <josee.souzaaa@gmail.com>
2022-06-21 21:08:58 -0300

    Requisição + formatação da data

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,5 +1,8 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 interface LessonsProps {
   title: string
   slug: string
@@ -8,11 +11,16 @@ interface LessonsProps {
 }
 
 export function Lesson(props: LessonsProps) {
-  const isLessonAvailable = false
+  const isLessonAvailable = isPast(props.availableAt)
+  const availableDateFormatted = format(
+    props.availableAt,
+    "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
+    { locale: ptBR }
+  )
 
   return (
     <a href="#">
-      <span className="text-gray-300">{props.availableAt.toString()}</span>
+      <span className="text-gray-300">{availableDateFormatted}</span>
 
       <div className="rounded border border-gray-500 p-4 mt-2">
         <header className="flex items-center justify-between">
