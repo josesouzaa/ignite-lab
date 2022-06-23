c9416d4345d5aea63825fbfdea81fc430ad98967
josesouzaa <josee.souzaaa@gmail.com>
2022-06-23 19:41:07 -0300

    Layout página de inscrição

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.png)'
+      },
       fontFamily: {
         sans: 'Roboto, sans-serif'
       },
